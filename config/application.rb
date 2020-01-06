1d75ee638532ec6d8f5a68931c8ca910a73cee8a
Taka0010 <takayuki.yamato@gmail.com>
2020-01-06 15:11:38 +0900

    branch-test
    ブランチのテストをしました。

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+#ブランチテスト
 Bundler.require(*Rails.groups)
 
 module PersonalApp
