f6966145e80076385e8d1c289ad1cb6b3e761051
Taka0010 <takayuki.yamato@gmail.com>
2020-01-18 18:58:47 +0900

    test
    test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:image, :message, :tag).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :tag, :message).merge(user_id: current_user.id)
   end
 
   def set_post
